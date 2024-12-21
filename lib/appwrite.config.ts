b32739e7af5c2a71a1a491a462a8f994948be216
Maamoon Salman <137584309+maamoonzsalman@users.noreply.github.com>
2024-12-21 10:22:55 -0800

    fixed user creation logic

--- lib/appwrite.config.ts
+++ lib/appwrite.config.ts
@@ -1,23 +1,20 @@
 import * as sdk from 'node-appwrite';
 
-const {
-    NEXT_PUBLIC_PROJECT_ID,
-    NEXT_PUBLIC_API_KEY,
-    DATABASE_ID,
-    PATIENT_COLLECTION_ID,
-    DOCTOR_COLLECTION_ID,
-    APPOINTMENT_COLLECTION_ID,
-    NEXT_PUBLIC_BUCKET_ID: BUCKET_ID,
-    NEXT_PUBLIC_ENDPOINT: ENDPOINT,
-
-} = process.env;
+export const PROJECT_ID = process.env.NEXT_PUBLIC_PROJECT_ID as string;
+export const API_KEY = process.env.NEXT_PUBLIC_API_KEY as string;
+export const DATABASE_ID = process.env.DATABASE_ID as string;
+export const PATIENT_COLLECTION_ID = process.env.PATIENT_COLLECTION_ID as string;
+export const DOCTOR_COLLECTION_ID = process.env.DOCTOR_COLLECTION_ID as string;
+export const APPOINTMENT_COLLECTION_ID = process.env.APPOINTMENT_COLLECTION_ID as string;
+export const BUCKET_ID = process.env.NEXT_PUBLIC_BUCKET_ID as string;
+export const ENDPOINT = process.env.NEXT_PUBLIC_ENDPOINT as string;
 
 const client = new sdk.Client();
 
 client
-    .setEndpoint(process.env.NEXT_PUBLIC_ENDPOINT as string)
-    .setProject(process.env.NEXT_PUBLIC_PROJECT_ID as string)
-    .setKey(process.env.NEXT_PUBLIC_API_KEY as string);
+    .setEndpoint(ENDPOINT)
+    .setProject(PROJECT_ID)
+    .setKey(API_KEY);
 
 export const databases = new sdk.Databases(client);
 export const storage = new sdk.Storage(client);
